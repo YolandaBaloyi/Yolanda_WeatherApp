1026dc26eac9241a9dbe71c9c8e793e5287b8335
YolandaBaloyi <Yolandabaloyi16@gmail.com>
2024-01-12 13:46:22 +0200

    Added some APIs and made the website functional

--- src/app.js
+++ src/app.js
@@ -5,13 +5,14 @@ let humidityElement=document.querySelector("#humidity");
 let windSpeedElement=document.querySelector("#wind-speed");
 let timeElement=document.querySelector("#time");
 let date= new Date(response.data.time * 1000);
-
+let iconElement = document.querySelector("#icon");
 
 let cityElement=document.querySelector("#city");
 cityElement.innerHTML=response.data.city;
 temperatureElement.innerHTML= Math.round(temperature);
 let descriptionElement=document.querySelector("#description");
  
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
     console.log(response.data);
 
     descriptionElement.innerHTML=response.data.condition.description;
@@ -19,6 +20,8 @@ let descriptionElement=document.querySelector("#description");
     windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
 timeElement.innerHTML=formalDate(date);
 
+
+getForecast(response.data.city);
 }
 function formalDate(date){
 
@@ -63,3 +66,48 @@ let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Pretoria")
+
+function getForecast(city) {
+    let apiKey = "46a3a2bt13e461baff9od195b7a890f2";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+  }
+
+
+  function displayForecast(response) {
+    let forecastHtml = "";
+  
+    response.data.daily.forEach(function (day, index) {
+      if (index < 5) {
+        forecastHtml =
+          forecastHtml +
+          `
+        <div class="weather-forecast-day">
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
+            </div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}º</div>
+          </div>
+        </div>
+      `;
+      }
+    });
+  
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+  }
+
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+    return days[date.getDay()];
+  }
+
+  displayForecast();
